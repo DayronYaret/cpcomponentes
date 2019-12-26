41d747db313750fdb9e5d6c8bb1f4e8bcfa5f433
carlosmesac <carlosmesac98@gmail.com>
2019-12-26 10:49:16 +0000

    modificada pantalla principal admin y add product

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,13 +1,13 @@
-<!DOCTYPE html>
-<head>
-    <title>Administrador</title>
+@extends("navBar")
 
 </head>
-<body>
-<a href="/products"><button>Ver productos</button></a>
-<a href="/add-product"><button>Añadir producto</button></a>
-<a href="/transactions"><button>Ver transacciones</button></a>
-<a href="/orders"><button>Ver ordenes</button></a>
+<body >
+<section style="margin: 7%">
+<a href="/products"><button class="btn btn-outline-dark btn-block d-block">Ver productos</button></a>
+<a href="/add-product"><button class="btn btn-outline-dark btn-block d-block">Añadir producto</button></a>
+<a href="/transactions"><button class="btn btn-outline-dark btn-block d-block">Ver transacciones</button></a>
+<a href="/orders"><button class="btn btn-outline-dark btn-block d-block">Ver ordenes</button></a>
+</section>
 </body>
 
 </html>
